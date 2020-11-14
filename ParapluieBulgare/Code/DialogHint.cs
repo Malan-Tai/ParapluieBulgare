ace0a26641a82fa65825bd0d1d83804c4dae8a65
Malan-Tai <malantai99@gmail.com>
2020-11-14 13:45:55 +0100

    Started working on hints

---
+++ ParapluieBulgare/Code/DialogHint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParapluieBulgare.Code
+{
+    class DialogHint : Hint
+    {
+        public string Sentence { get; set; }
+
+        public DialogHint(string text, string sentence) : base(text)
+        {
+            Sentence = sentence;
+        }
+    }
+}
---
+++ ParapluieBulgare/Code/Hint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParapluieBulgare.Code
+{
+    class Hint
+    {
+        public string Text { get; set; }
+
+        public Hint(string text)
+        {
+            Text = text;
+        }
+    }
+}
