0fb86e695d501534fc0b41b4d8dfa0940cec2f8e
Malan-Tai <malantai99@gmail.com>
2020-11-15 01:19:58 +0100

    Began furniture

--- ParapluieBulgare/Game1.cs
+++ ParapluieBulgare/Game1.cs
@@ -34,7 +34,7 @@ namespace ParapluieBulgare
             "Directeur_idle",
             "Journaliste_IDLE",
             "rando_fem_idle",
-            "background",
+            "Background",
             "vigile_walk_3",
             "Ascenseur"
         };
@@ -92,12 +92,12 @@ namespace ParapluieBulgare
             player = new Player(GetAnimation("MC_Walk_SpriteSheet"), GetAnimation("MC_Walk_SpriteSheet"), white);
             floors = new Floor[]
             {
-                new Floor(player, -1, textureDict["background"], GetFloorNPCs(-1)),
-                new Floor(player, 0, textureDict["background"], GetFloorNPCs(0)),
-                new Floor(player, 1, textureDict["background"], GetFloorNPCs(1)),
-                new Floor(player, 2, textureDict["background"], GetFloorNPCs(2)),
-                new Floor(player, 3, textureDict["background"], GetFloorNPCs(3)),
-                new Floor(player, 4, textureDict["background"], GetFloorNPCs(4))
+                new Floor(player, -1, textureDict["Background"], GetFloorNPCs(-1)),
+                new Floor(player, 0, textureDict["Background"], GetFloorNPCs(0)),
+                new Floor(player, 1, textureDict["Background"], GetFloorNPCs(1)),
+                new Floor(player, 2, textureDict["Background"], GetFloorNPCs(2)),
+                new Floor(player, 3, textureDict["Background"], GetFloorNPCs(3)),
+                new Floor(player, 4, textureDict["Background"], GetFloorNPCs(4))
             };
 
             guards = new List<Guard>
